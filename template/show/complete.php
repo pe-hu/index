8af7ac551bf62bc22bc918b979a4f03709d29ae6
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-13 20:09:38 +0900

    cm

---
+++ template/show/complete.php
@@ -0,0 +1,95 @@
+<?php
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+$title = (string)filter_input(INPUT_POST, 'title');
+$contents = (string)filter_input(INPUT_POST, 'contents');
+$tag = (string)filter_input(INPUT_POST, 'tag');
+$url = (string)filter_input(INPUT_POST, 'url');
+$link = (string)filter_input(INPUT_POST, 'link');
+
+$fp = fopen('think.csv', 'a+b');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$title, $contents, $tag, $url, $link,]);
+    rewind($fp);
+}
+
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
+flock($fp, LOCK_UN);
+fclose($fp);
+
+?>
+<html lang="ja">
+<head>
+<title> Done | ORG </title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta http-equiv="refresh" content="1;URL=submit.php">
+<style type="text/css">
+html, body {padding:0; margin:0;}
+#foot {
+  display:block;
+  position:relative;
+  top:0; left:0;
+  width:100%;
+  height:100vh;
+}
+#foot .inside h1 {
+  width:50vw;
+  position:absolute;
+  top:47.5%; left:50%;
+  padding:0; margin:0;
+  transform: translate(-50%, -50%);
+  font-size: 10vw; font-weight:500;
+  font-family: "SF Compact", sans-serif;
+}
+#foot .inside p {
+  font-size:2.5vw;
+  width:100%;
+  text-align:center;
+  position:absolute;
+  top:90%; left:50%;
+  transform: translate(-50%, -50%);
+  font-family: "SF Compact", sans-serif;
+}
+#foot .inside b {
+  border:0.25vw solid;
+  background:#fff;
+  padding:0.5vw 2.5vw;
+  border-radius:2rem;
+}
+
+</style>
+</head>
+<body>
+<div id="foot">
+<div class="inside">
+<h1><span id="rename"></span></h1>
+<p class="notice"><b>ご投稿ありがとうございました</b></p>
+</div>
+</div>
+<script>
+var text = ["Thank You","for", "Submit" ];
+var counter = 0;
+var elem = document.getElementById("rename");
+var inst = setInterval(change, 750);
+
+elem.innerHTML = text[counter];
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+  }
+};
+</script>
+
+</body>
+</body>
+</html>
---
+++ template/show/submit.php
@@ -0,0 +1,72 @@
+<?php
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+$title = (string)filter_input(INPUT_POST, 'title');
+$contents = (string)filter_input(INPUT_POST, 'contents');
+$tag = (string)filter_input(INPUT_POST, 'tag');
+$url = (string)filter_input(INPUT_POST, 'url');
+$link = (string)filter_input(INPUT_POST, 'link');
+
+$fp = fopen('think.csv', 'a+b');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$title, $contents, $tag, $url, $link,]);
+    rewind($fp);
+}
+
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
+flock($fp, LOCK_UN);
+fclose($fp);
+
+?>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title> Submit | ORG </title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(function(){
+$("#").load("");
+})
+</script>
+<link rel="stylesheet" href="book.css"/>
+<style type="text/css">
+</style>
+</head>
+<body>
+<div id="header">
+<a href="../show/" target="_parent">Show</a>
+<a href="submit.php" target="_parent">Submit</a>
+</div>
+<form action="complete.php" id="org" method="post" target="_parent">
+<p><input type="text" name="title" placeholder="title" required></p>
+<div class="search-box tag">
+<ul>
+<li>
+<input type="radio" name="tag" value="one" id="one">
+<label for="one" class="label">1</label></li>
+<li>
+<input type="radio" name="tag" value="two" id="two">
+<label for="two" class="label">2</label></li>
+</ul>
+<p>Link
+<select name="link" required>
+<option value="block">Yes</option>
+<option value="none">None</option>
+</select><br/>
+<input type="url" name="url" placeholder="URL"></p>
+</select></p>
+<p><textarea name="contents" placeholder="コメント" required></textarea></p>
+</div>
+<div class="reset">
+<button type="submit">Submit | 投稿する</button>
+</div>
+</form>
+</body>
+</html>
