f93d702f03d683bb0522ec25f2eaadc56b11c543
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-13 21:28:51 +0900

    aa

--- aa/works/index.php
+++ aa/works/index.php
@@ -33,105 +33,103 @@ flock($fp, LOCK_UN);
 fclose($fp);
 
 ?>
-<!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" type="text/css" href="/css/calendar.css"/>
-<link rel="stylesheet" type="text/css" href="/css/menu.css"/>
-<link rel="stylesheet" type="text/css" href="/css/welcome.css"/>
-<link rel="stylesheet" type="text/css" href="/jp/cm/font/fonts.css"/>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>design | ayumi akutagawa</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+<script src="org.js"></script>
 <script type="text/javascript">
 $(function(){
 $("#menu").load("/jp/cm/aa/menu.html");
+$("#cv_link").load("/jp/cm/aa/cv/link.php");
 })
 </script>
+<script type="text/javascript">
+</script>
+<link rel="stylesheet" type="text/css" href="/jp/cm/show/book.css"/>
+<link rel="stylesheet" type="text/css" href="/jp/cm/cv/greating.css"/>
+<link rel="stylesheet" type="text/css" href="/jp/cm/font/fonts.css"/>
 <style type="text/css">
-#calendar h1,
-#calendar .date,
-#calendar .title
+#org h1,
+.list .date,
+.list .title
 {font-family: flower;}
-#calendar div u {text-decoration:none;}
-#calendar div .date {
+.list .date {
   font-size:125%;
-  padding:0 2.5%;
+  padding:0 2.5% 0 0;
   width:20%;
+  display:inline-block;
   float:left;
   pointer-events: none;
 }
-#calendar div p {
-  position:absolute;
-  z-index:2;
-  padding:0; margin:0;
-  display:inline-block;
-}
-#calendar div .title {
+.list .title {
   font-size:150%;
   width:75%;
   right:0;
+  display:inline-block;
   white-space:nowrap;
   text-decoration:none;
   pointer-events: none;
 }
-#calendar .info
-{
-  font-family:secretcrane;
-  letter-spacing:1rem;
-  font-style:normal;
-}
-#calendar .info span {
-  width: 7.5rem; height:7.5rem;
-  margin:0 0.5rem 0.5rem;
+.list p span {
+  width: 10vw;
+  margin:0 5vw 0 0;
   position: relative;
   display: inline-block;
 }
-#calendar .info img {width: 100%;}
-#calendar div a {color: #000;}
-#calendar div a:hover {zoom:2.5; background-color:rgba(255,190,200,0.5);}
-
-#calendar div .none {pointer-events:none;}
-#calendar .info .none {display:none;}
-
+.list p span img {width: 100%;}
+.list li:hover {
+  transform:scale(1.5)
+}
 </style>
-<title>works | ayumi akutagawa</title>
 </head>
 <body>
 <div id="menu"></div>
-<div id="calendar" class="">
-<h1>works</h1>
+
+<div id="greating">
+<form id="org">
+<h1>design</h1>
+</form>
+
+<ul class="list">
 <?php if (!empty($rows)): ?>
 <?php foreach ($rows as $row): ?>
-<div>
-<p class="date"><?=h($row[0])?></p>
-<p class="title"><u><?=h($row[1])?></u></p>
-<a class="<?=h($row[2])?>" href="<?=h($row[2])?>" target="_blank" rel="noopener noreferrer">
-<marquee class="info" scrollamount="15">
-<span class="<?=h($row[3])?>"><img src="<?=h($row[4])?>"></span>
-<span class="<?=h($row[5])?>"><img src="<?=h($row[6])?>"></span>
-<span class="<?=h($row[7])?>"><img src="<?=h($row[8])?>"></span>
-<span class="<?=h($row[9])?>"><img src="<?=h($row[10])?>"></span>
-<span class="<?=h($row[11])?>"><img src="<?=h($row[12])?>"></span>
-</marquee>
-</a>
-</div>
+<li class="list_item list_toggle" data-tag="<?=h($row[2])?>">
+<span>
+  <b class="date"><?=h($row[0])?></b>
+  <u class="title"><?=h($row[1])?></u>
+</span>
+<p>
+<span style="display: <?=h($row[3])?>;"><img src="<?=h($row[4])?>"></span>
+<span style="display: <?=h($row[5])?>;"><img src="<?=h($row[6])?>"></span>
+<span style="display: <?=h($row[7])?>;"><img src="<?=h($row[8])?>"></span>
+<span style="display: <?=h($row[9])?>;"><img src="<?=h($row[10])?>"></span>
+<span style="display: <?=h($row[11])?>;"><img src="<?=h($row[12])?>"></span>
+</p>
+<a style="display: <?=h($row[3])?>;" href="<?=h($row[2])?>" target="_blank" rel="noopener noreferrer"></a>
+</li>
 <?php endforeach; ?>
 <?php else: ?>
-<div>
-<p class="date">date</p>
-<p class="title"><u>title</u></p>
-<a class="none" href="<?=h($row[2])?>" target="_blank" rel="noopener noreferrer">
-<marquee class="info" scrollamount="15">
-images
-</marquee>
-</a>
-</div>
+<li class="list_item list_toggle" data-tag="<?=h($row[2])?>">
+<span>
+  <b class="date">Title</b>
+  <u class="title">contents</u>
+</span>
+<p>
+<span style="display:have;"><img src="http://vg.pe.hu/jp/logo.png"></span>
+<span style="display:have;"><img src="http://vg.pe.hu/jp/logo.png"></span>
+<span style="display:have;"><img src="http://vg.pe.hu/jp/logo.png"></span>
+<span style="display:none;"><img src="http://vg.pe.hu/jp/logo.png"></span>
+<span style="display:none;"><img src="http://vg.pe.hu/jp/logo.png"></span>
+</p>
+<a style="display:<?=h($row[3])?>;" href="<?=h($row[2])?>" target="_blank" rel="noopener noreferrer"></a>
+</li>
 <?php endif; ?>
-</div>
+</ul>
+<div id="cv_link"></div>
 </div>
 </body>
 </html>
