6d96f15131d46b534c0ac0c406abde32512298f4
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-18 02:20:00 +0900

    pehu

---
+++ link.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="/css/greating.css"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(function(){
+$("#").load("");
+})
+</script>
+<title>∧° ┐ | creative, community space</title>
+<body>
+<div id="cv_link">
+<p><b>Email</b>we.are.pe.hu@gmail.com</p>
+<p><b>Matrix</b>
+<a href="https://twitter.com/pe_hu_" target="_blank" rel="noopener noreferrer">Twitter</a>
+<a href="https://www.instagram.com/pe_hu_/" target="_blank" rel="noopener noreferrer">Instagram</a>
+<a href="https://www.youtube.com/channel/UCkTlW5dLa_DSPZYauh8xKcQ" target="_blank" rel="noopener noreferrer">YouTube</a>
+</p>
+<p><b>Projects</b>
+<a href="http://vg.pe.hu/publication/value/" target="_blank" rel="noopener noreferrer">大切にすることを大切にする</a>
+<a href="http://ichoose.pe.hu/" target="_blank" rel="noopener noreferrer">ichoose</a>
+<a href="http://newlifecollection.com/" target="_blank" rel="noopener noreferrer">New Life Collection</a>
+<a href="http://chottocrazy.pe.hu/" target="_blank" rel="noopener noreferrer">大 chotto crazy</a>
+<a href="http://praytimes.pe.hu/" target="_blank" rel="noopener noreferrer">プレイタイムズ</a>
+<a href="http://newlifecollection.pe.hu/" target="_blank" rel="noopener noreferrer">新しい生活を集めること</a>
+<a href="http://goout.pe.hu/" target="_blank" rel="noopener noreferrer">Go Out</a>
+</p>
+<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<div id="mc_embed_signup" style="padding-top:0.25vw;">
+<form action="https://gmail.us20.list-manage.com/subscribe/post?u=3b5bd5ca5fc54e4c3847e9603&amp;id=103583076f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+<div id="mc_embed_signup_scroll">
+<div class="mc-field-group">
+	<label for="mce-EMAIL"><b>Mailing List</b></label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3b5bd5ca5fc54e4c3847e9603_103583076f" tabindex="-1" value=""></div>
+</div>
+</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
+<a href="https://gossipsweb.net"><img src="https://gossipsweb.net/images/twwmwh-blue.jpg" alt="This website was made with hands." style="width: 25vw; border: 1px solid #000;" /></a>
+</div>
+</body>
+</html>
---
+++ update/nlc.php
@@ -0,0 +1,74 @@
+<?php
+
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+$type = (string)filter_input(INPUT_POST, 'type'); // $_POST['type']
+$info = (string)filter_input(INPUT_POST, 'onfo'); // $_POST['info']
+$url = (string)filter_input(INPUT_POST, 'url'); // $_POST['url']
+
+$fp = fopen('nlc.csv', 'a+b');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$type, $info, $url]);
+    rewind($fp);
+}
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
+flock($fp, LOCK_UN);
+fclose($fp);
+
+?>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width">
+<script type="text/javascript">
+$(function(){
+})
+</script>
+<title>Update | New Life Collection</title>
+<style>
+#other {
+  font-size: 2vw; padding:2.5% 0;
+  font-family: "YuGothic","Yu Gothic","游ゴシック体";
+}
+#other span {
+  display:inline;
+  text-decoration:none;
+  padding:0.5vw 1vw;
+  filter: blur(1);
+  -webkit-filter: blur(1);
+  transition: all 1500ms ease;
+}
+
+#other span {
+  line-height:222%;
+  margin:0.5vw 1vw 0.5vw 0;
+}
+#other .popup {
+  border:1px solid #000;
+}
+#other .website {
+  color:#000;
+  background:#eee;
+}
+</style>
+</head>
+<body>
+<div id="other">
+<?php if (!empty($rows)): ?>
+<?php foreach ($rows as $row): ?>
+<span class="<?=h($row[0])?>">
+<?=h($row[1])?>
+</span>
+<?php endforeach; ?>
+<?php else: ?>
+<?php endif; ?>
+</div>
+</body>
+</html>
