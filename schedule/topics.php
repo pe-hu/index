65a1e7c73c67d00c2158e661870f99da9b6750e5
∧° ┐ <we.are.pe.hu@gmail.com>
2022-06-01 17:39:13 +0900

    Update topics.php

--- schedule/topics.php
+++ schedule/topics.php
@@ -195,6 +195,8 @@ fclose($fp);
   <label class="refine-btn upcoming" for="refine-3">UPCOMING</label>
   <input id="refine-5" type="radio" name="refine-btn"><span class="refine-5"><b>✔</b></span>
   <label class="refine-btn appointment" for="refine-5">BY APPOINTMENT</label>
+  <input id="refine-8" type="radio" name="refine-btn"><span class="refine-8"><b>✔</b></span>
+  <label class="refine-btn members" for="refine-8">MEMBERS ONLY</label>
   <br/>
   <input id="refine-6" type="radio" name="refine-btn"><span class="refine-6"><b>✔</b></span>
   <label class="refine-btn community" for="refine-6">COMMUNITY EVENT</label>
@@ -206,8 +208,6 @@ fclose($fp);
   <label class="refine-btn residency" for="refine-7">OPEN STUDIO</label>
   <input id="refine-4" type="radio" name="refine-btn"><span class="refine-4"><b>✔</b></span>
   <label class="refine-btn shop" for="refine-4">POP-UP STORE</label>
-  <input id="refine-8" type="radio" name="refine-btn"><span class="refine-8"><b>✔</b></span>
-  <label class="refine-btn members" for="refine-8">MEMBERS ONLY</label>
 <hr/>
 <?php if (!empty($rows)): ?>
 <?php foreach ($rows as $row): ?>
