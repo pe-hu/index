39faf64ca1510bb9762b8edfdf19ef17087261b6
∧° ┐ <we.are.pe.hu@gmail.com>
2022-06-01 16:26:55 +0900

    pehu

--- schedule/topics.php
+++ schedule/topics.php
@@ -199,6 +199,8 @@ fclose($fp);
   <label class="refine-btn live" for="refine-2">LIVE/PERFORMANCE</label>
   <input id="refine-4" type="radio" name="refine-btn"><span class="refine-4"><b>✔</b></span>
   <label class="refine-btn shop" for="refine-4">POP-UP STORE</label>
+  <input id="refine-7" type="radio" name="refine-btn"><span class="refine-7"><b>✔</b></span>
+  <label class="refine-btn residency" for="refine-7">RESIDENCY</label>
   <input id="refine-8" type="radio" name="refine-btn"><span class="refine-8"><b>✔</b></span>
   <label class="refine-btn members" for="refine-8">MEMBERS ONLY</label>
 <hr/>
