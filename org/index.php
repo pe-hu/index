afe611b6f06d567f537e6fa3db9cac7ff3ed5043
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 08:18:38 +0900

    Update index.php

--- org/index.php
+++ org/index.php
@@ -311,7 +311,6 @@ fclose($fp);
     </footer>
 
     <script type="text/javascript ">
-        
         var Greeting = [
             "物理的なもの、デジタルコンテンツ、権利や資格など、ペフが所有する全てのものをここに記録します。",
             "Natalia Panzerのプロジェクト「The Things I Own」を真似て、所有するものを「購入物」「頂き物」「無料配布物」「制作物」「共同制作物」「その他」「販売物」に分類します。"
