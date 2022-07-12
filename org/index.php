3bda3686b498e8589ff58fd3a68192804f794e0b
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 02:56:40 +0900

    Update index.php

--- org/index.php
+++ org/index.php
@@ -156,7 +156,7 @@ fclose($fp);
         
         @media screen and (max-width: 750px) {
             #catalog {
-                top: 40%;
+                top: 42.5%;
             }
             #catalog .list_item {
                 position: relative;
