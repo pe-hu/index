2bf02a14dd9374d51419b8cd623c31fea2981a1b
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 08:20:57 +0900

    Update index.php

--- org/index.php
+++ org/index.php
@@ -258,7 +258,9 @@ fclose($fp);
         <ul class="mousedragscrollable">
             <li id="images" class="collection">
                 <p id="greeting">
-                    <script>$('#greeting').html(Greeting);</script>
+                    <script>
+                    $('#greeting').html(Greeting);
+                    </script>
                 </p>
                 <ol id="catalog" class="org">
                     <?php if (!empty($rows)): ?>
@@ -311,7 +313,9 @@ fclose($fp);
     </footer>
 
     <script type="text/javascript ">
+        
         var Greeting = [
+            "会期：2022年7月23日（土）〜 8月21日（日） | 会場：BnA Alter Museum",
             "物理的なもの、デジタルコンテンツ、権利や資格など、ペフが所有する全てのものをここに記録します。",
             "Natalia Panzerのプロジェクト「The Things I Own」を真似て、所有するものを「購入物」「頂き物」「無料配布物」「制作物」「共同制作物」「その他」「販売物」に分類します。"
         ]
