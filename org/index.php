71ad012fc832a2551dcc1196898beedc0389cec9
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 07:56:40 +0900

    Update index.php

--- org/index.php
+++ org/index.php
@@ -249,7 +249,7 @@ fclose($fp);
     <main id="main">
         <ul class="mousedragscrollable">
             <li id="images" class="collection">
-                <p id="greeting"></p>
+                <p id="greeting"><script>$('#greeting').html(Greeting);</script></p>
                 <ol id="catalog" class="org">
                     <?php if (!empty($rows)): ?>
                     <?php foreach ($rows as $row): ?>
@@ -337,20 +337,6 @@ fclose($fp);
             let randNote = Math.floor(Math.random() * notes.length);
             synth.triggerAttackRelease(notes[randNote], "6n");
         });
-
-        var Greeting = [
-            "会期：2022年7月23日（土）〜 8月21日（日） | 会場：BnA Alter Museum",
-            "物理的なもの、デジタルコンテンツ、権利や資格など、ペフが所有する全てのものをここに記録します。",
-            "Natalia Panzerのプロジェクト「The Things I Own」を真似て、所有するものを「購入物」「頂き物」「無料配布物」「制作物」「共同制作物」「その他」「販売物」に分類します。"
-        ]
-
-        function func1() {
-            $('#greeting').html(Greeting);
-        }
-
-        function more() {
-            $("#header marquee").html(Greeting[Math.floor(Math.random() * Greeting.length)]);
-        }
     </script>
 </body>
 
