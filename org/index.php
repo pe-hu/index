eccddd77739ac4835dbcd322255045387d532f84
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 05:30:04 +0900

    Update index.php

--- org/index.php
+++ org/index.php
@@ -162,7 +162,7 @@ fclose($fp);
         
         @media screen and (max-width: 1000px) {
             #catalog {
-                top: 45%;
+                top: 50%;
             }
             #catalog .list_item {
                 position: relative;
