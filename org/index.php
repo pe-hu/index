2fbafc064b821f314b5d2a8b025a6deea66b2ac5
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 05:56:34 +0900

    org

--- org/index.php
+++ org/index.php
@@ -31,7 +31,7 @@ fclose($fp);
     <title>Things That I (We) Own, in 3D</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="images.css" />
+    <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="/org/style.css" />
     <link rel="stylesheet" href="/org/searchBox.css" />
     <style>
