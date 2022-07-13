e40b8ed8efb308a3878518623f589e58bea7d259
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 10:58:26 +0900

    org

--- org/index.php
+++ org/index.php
@@ -213,7 +213,6 @@ fclose($fp);
         }
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="index.js"></script>
     <script src="/org/searchBox.js"></script>
     <script src="/www/scrollable.js"></script>
     <script src="https://creative-community.space/coding/js/tone/jquery.min.js"></script>
@@ -333,6 +332,20 @@ fclose($fp);
     </footer>
 
     <script type="text/javascript ">
+    var Greeting = [
+        "２０２２年、夏休みの自由研究 ◎ ",
+        "これは、ペフが所有する全てのものを集めたコレクションです。",
+        "出版物・制作物、ウェブドメイン・デジタルツール、メディアファイルなど、あらゆる形態・形式のものを、このコレクションに記録します。",
+        "Natalia Panzerのプロジェクト「The Things I Own」を真似て、所有するものを「購入物」「頂き物」「無料配布物」「制作物」「共同制作物」「その他」「販売物」に分類します。"
+    ]
+
+    function greeting() {
+        $("#text").html(Greeting);
+    }
+
+    function more() {
+        $("#header marquee").html(Greeting[Math.floor(Math.random() * Greeting.length)]);
+    }
 
         $('a[href^="# "]').click(function() {
             var href = $(this).attr("href ");
