671eea14a73b5ceca8826349a5b6461cf38a70fd
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 18:30:41 +0900

    Update index.php

--- org/index.php
+++ org/index.php
@@ -378,7 +378,7 @@ fclose($fp);
             ?>
           </span>
           <br/>
-          <span>URL: </span>
+          <span>URL : </span>
           <a class="cc_style" href="<?php echo $_SERVER['REQUEST_URI'];?>">
           <?php
           echo $_SERVER['SERVER_NAME'];
