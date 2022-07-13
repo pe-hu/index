8b91eef9e1f88f1e149c700e7e1315b7aa2950c4
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-13 12:05:47 +0900

    org

--- org/index.js
+++ org/index.js
@@ -1,8 +1,16 @@
 var Greeting = [
-    "２０２２年、夏休みの自由研究 ◎ ",
-    "これは、ペフが所有する全てのものを集めたコレクションです。",
-    "出版物・制作物、ウェブドメイン・デジタルツール、メディアファイルなど、あらゆる形態・形式のものを、このコレクションに記録します。",
-    "Natalia Panzerのプロジェクト「The Things I Own」を真似て、所有するものを「購入物」「頂き物」「無料配布物」「制作物」「共同制作物」「その他」「販売物」に分類します。"
+    "BnA Alter Museum　◎　２０２２年の夏休み　◎　ペフの自由研究　◎　",
+    "私（わたしたち）が所有するすべてのものを発表する展覧会「The Things I (We) Own, in 3D」を開催します。",
+    "　◎　デジタル／フィジカル・販売品／非売品・新品／中古など、形態／状態を問わず所有するもの全てをここに発表します。　◎　",
+    "　※　",
+    ""
+]
+
+var Index = [
+    "出版物・制作物、ウェブドメイン・デジタルツール、メディアファイルなど、私（わたしたち）が所有するあらゆる形態・形式のものを、このページに記録します。",
+    "所有するものを、カテゴリー毎のCSVファイルに記録し、ページ内に埋め込んだPHPを使って表示します。",
+    "Natalia Panzerのプロジェクト「The Things I Own」を真似て、所有するものを「購入物」「頂き物」「無料配布物」「制作物」「共同制作物」「その他」「販売物」に分類します。",
+    "このページは、Chiaのウェブサイト「ifyouknewmewouldyoulove.me」を真似て制作しました。"
 ]
 
 function greeting() {
@@ -10,5 +18,5 @@ function greeting() {
 }
 
 function more() {
-    $("#header marquee").html(Greeting[Math.floor(Math.random() * Greeting.length)]);
+    $("#header marquee").html(Index[Math.floor(Math.random() * Index.length)]);
 }
