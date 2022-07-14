2c19bca9d9792a642bca46ab57e9a841e4feb736
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-14 22:29:39 +0900

    org

--- org/img.php
+++ org/img.php
@@ -28,7 +28,7 @@ fclose($fp);
     <html lang="ja">
 
     <head>
-        <title>P E H U is | Things that I (We) owned</title>
+        <title>Things that I (We) owned, in 3D</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="stylesheet.css" />
