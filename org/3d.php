b2c286d1e5b775108184e28fa6bd7cf3f8fb787d
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-14 20:56:58 +0900

    Update 3d.php

--- org/3d.php
+++ org/3d.php
@@ -356,7 +356,7 @@ fclose($fp);
 
     <main id="main">
         <ul class="mousedragscrollable">
-            <li id="img" class="collection"></li>
+            <li id="img" class=""></li>
             <li id="images" class="collection">
                 <div id="greeting">
                     <p class="nlc_style" id="text" class="tab"></p>
