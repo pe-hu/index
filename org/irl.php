9a981c1592267b92433da04f9f7e83f45d1e7ac9
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-15 02:45:09 +0900

    Update irl.php

--- org/irl.php
+++ org/irl.php
@@ -64,7 +64,7 @@ fclose($fp);
         header marquee:hover {
             cursor: pointer;
             color: #fff;
-            background: #aaa;
+            background: #ccc;
             font-style: italic;
         }
         
