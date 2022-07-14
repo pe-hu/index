762d0a4aebf634841b9fd2ed2b410b4980562ca0
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-14 17:01:08 +0900

    Update img.php

--- org/img.php
+++ org/img.php
@@ -59,7 +59,7 @@ fclose($fp);
         <ol id="catalog" class="org">
             <?php if (!empty($rows)): ?>
             <?php foreach ($rows as $row): ?>
-            <li class="list_item list_toggle <?=h($row[1])?>" data-org="<?=h($row[0])?>">
+            <li class="list_item list_toggle" data-org="<?=h($row[0])?>">
                 <img src="<?=h($row[2])?>">
                 <p><b><?=h($row[3])?></b></p>
                 <p>
