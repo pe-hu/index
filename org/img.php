9902065b5662266673c559b29487388604e6a2c6
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-14 17:06:14 +0900

    org

--- org/img.php
+++ org/img.php
@@ -24,46 +24,64 @@ fclose($fp);
 
 ?>
 
-    <!DOCTYPE html>
-    <html lang="ja">
-
-    <head>
-        <title>P E H U is | Things that I (We) owned</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="stylesheet.css" />
-        <style>
-            body,
-            ol,
-            li {
-                padding: 0;
-                margin: 0;
-            }
-            
-            #catalog li {
-                margin: 2.5vw;
-                width: 10rem;
-                max-width: 90vw;
-                max-height: 90vw;
-            }
-            
-            #catalog li img {
-                max-width: 100%;
-                max-height: 100%;
-            }
-        </style>
-    </head>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>P E H U is | Things that I (We) owned</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css" />
+    <style>
+        body,
+        ol,
+        li {
+            padding: 0;
+            margin: 0;
+        }
+        
+        #catalog li {
+            position: relative;
+            padding: 0;
+            margin: 2.5vw;
+            width: 7.5rem;
+            height: 7.5rem;
+            max-width: 90vw;
+            transition: all 500ms ease;
+        }
+        
+        #catalog li:hover {
+            width: 750px;
+            max-width: 90vw;
+            height: 750px;
+            max-height: 90vw;
+        }
+        
+        #catalog li img {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+        
+        #catalog li p {
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
+    </style>
+</head>
 
     <body>
 
         <ol id="catalog" class="org">
             <?php if (!empty($rows)): ?>
             <?php foreach ($rows as $row): ?>
-            <li class="list_item list_toggle" data-org="<?=h($row[0])?>">
+            <li class="list_item list_toggle <?=h($row[1])?>" data-org="<?=h($row[0])?>">
                 <img src="<?=h($row[2])?>">
+                <p><b><?=h($row[3])?></b></p>
                 <p>
-                    <b><?=h($row[3])?></b>
-                    <br><?=h($row[4])?>
+                    <?=h($row[4])?>
                 </p>
             </li>
             <?php endforeach; ?>
@@ -76,4 +94,4 @@ fclose($fp);
 
     </body>
 
-    </html>
+</html>
