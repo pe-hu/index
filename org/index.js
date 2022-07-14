376a94bc884e753f197b57b10b95f3512ac0b9ea
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-14 22:23:16 +0900

    org

---
+++ org/index.js
@@ -0,0 +1,10 @@
+var Index = [
+    "私（わたしたち）が所有する全てのもの（出版物・制作物、ウェブドメイン・デジタルツール、メディアファイルなど）を、このページに記録します。",
+    "Natalia Panzerのプロジェクト「The Things I Own」を真似て、所有するものを「購入物」「頂き物」「無料配布物」「制作物」「共同制作物」「その他」「販売物」に分類します。",
+    "カテゴリー毎のCSVファイルに所有するものを記録し、ページ内に埋め込んだPHPに品名を表示します。",
+    "このページは、Chiaのウェブサイト「ifyouknewmewouldyoulove.me」を真似て制作しました。"
+]
+
+function more() {
+    $("#header marquee").html(Index[Math.floor(Math.random() * Index.length)]);
+}
--- org/popup.js
+++ org/popup.js
@@ -7,17 +7,6 @@ var Greeting = [
     "｜　ビーエヌエーオルターミュージアムの周りで聞いた言葉を集めた地図を作る　｜　など"
 ]
 
-var Index = [
-    "私（わたしたち）が所有する全てのもの（出版物・制作物、ウェブドメイン・デジタルツール、メディアファイルなど）を、このページに記録します。",
-    "Natalia Panzerのプロジェクト「The Things I Own」を真似て、所有するものを「購入物」「頂き物」「無料配布物」「制作物」「共同制作物」「その他」「販売物」に分類します。",
-    "カテゴリー毎のCSVファイルに所有するものを記録し、ページ内に埋め込んだPHPに品名を表示します。",
-    "このページは、Chiaのウェブサイト「ifyouknewmewouldyoulove.me」を真似て制作しました。"
-]
-
 function greeting() {
     $("#greeting #text").html(Greeting);
-}
-
-function more() {
-    $("#header marquee").html(Index[Math.floor(Math.random() * Index.length)]);
 }
