09e5403a7a093e00abc20c53f8ed823675f39c1e
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-16 01:55:31 +0900

    Update irl.php

--- org/irl.php
+++ org/irl.php
@@ -161,86 +161,38 @@ fclose($fp);
             height: 100%;
             margin: 0;
         }
+            
+            #images {
+                position: relative;
+                width: 100%;
+                height: 100%;
+                margin: 0;
+                overflow: hidden;
+            }
+            
+            #images {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                width: 95%;
+                height: 0;
+                -webkit-transform: translate(-50%, -50%);
+                transform: translate(-50%, -50%);
+            }
+            
+            #images .list_item {
+                position: relative;
+                padding: 0;
+                margin: 2.5vh 0;
+            }
+            
+            #images img {
+                animation: 100s linear infinite spot;
+            }
 
         #motto {
             width: 27.5rem;
             max-width: 55%;
-        }
-        
-        body,
-        ol,
-        li {
-            padding: 0;
-            margin: 0;
-        }
-        
-        #collection {
-            display: -webkit-flex;
-            display: flex;
-            -webkit-justify-content: center;
-            justify-content: center;
-            -webkit-align-items: center;
-            align-items: center;
-            -webkit-flex-wrap: wrap;
-            flex-wrap: wrap;
-            list-style-type: none;
-            padding: 1rem 0 2.5rem;
-        }
-        
-        #collection li {
-            color: #333;
-            text-shadow: 0.1rem 0.1rem 0.1rem #fff;
-            font-size: 0.55rem;
-            position: relative;
-            padding: 0;
-            margin: 1vw;
-            width: 10rem;
-            height: 10rem;
-            max-width: 95vw;
-            max-height: 95vw;
-            transition: all 1000ms ease;
-        }
-        
-        #collection li p {
-            font-family: "ipag", monospace;
-            transform: scale(1, 1.25);
-            padding: 0.25rem;
-            margin: 0;
-            position: absolute;
-            z-index: 5;
-            bottom: 0;
-            left: 0;
-            pointer-events: none;
-            user-select: none;
-        }
-        
-        #collection li p b {
-            font-family: 'Times New Roman', serif;
-            font-weight: 500;
-            font-stretch: condensed;
-            font-variant: common-ligatures tabular-nums;
-            transform: scale(1, 1.1);
-            letter-spacing: -0.1rem;
-            word-spacing: -.1ch;
-            font-size: 150%;
-            display: inline-block;
-            padding: 0;
-            margin: 0.5rem 0;
-        }
-        
-        #collection li img {
-            width: 75%;
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            -webkit-transform: translate(-50%, -50%);
-            transform: translate(-50%, -50%);
-            pointer-events: none;
-            user-select: none;
-        }
-        
-        #collection li:hover p {
-            display: block;
         }
         
         @keyframes spot {
@@ -431,23 +383,19 @@ fclose($fp);
                             ?>
                     </p>
                 </div>
-                <ol id="collection" class="org">
-                    <?php if (!empty($rows)): ?>
+                    <ol id="images" class="org">
+                        <?php if (!empty($rows)): ?>
                         <?php foreach ($rows as $row): ?>
-                            <li class="list_item list_toggle" data-org="<?=h($row[0])?>">
+                        <li class="list_item list_toggle <?=h($row[1])?>" data-org="<?=h($row[0])?>">
                             <img src="<?=h($row[2])?>">
-                            <p>
-                                <b><?=h($row[3])?></b>
-                                <br/><?=h($row[4])?>
-                            </p>
                         </li>
                         <?php endforeach; ?>
-                    <?php else: ?>
+                        <?php else: ?>
                         <li class="list_item list_toggle min" data-org="test">
                             <img src="/logo.png">
                         </li>
-                    <?php endif; ?>
-                </ol>
+                        <?php endif; ?>
+                    </ol>
             </div>
         <ul class="mousedragscrollable">
             <li id="motto" class="collection"></li>
