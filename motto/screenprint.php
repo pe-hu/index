1debba340a6c296dd28fc451d601b88e5a8b8eb3
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-17 17:06:55 +0900

    Update screenprint.php

--- motto/screenprint.php
+++ motto/screenprint.php
@@ -333,9 +333,10 @@ fclose($fp);
         }
 
         .change #cover {
-            position: absolute;
+            position: fixed;
             top:0;
             left:0;
+            z-index: 0;
         }
 
         #footer,
