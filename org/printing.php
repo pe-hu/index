d8e24756a2b74f064df4486eec0ef1c242e3faf3
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-18 23:18:30 +0900

    org

--- org/printing.php
+++ org/printing.php
@@ -28,7 +28,7 @@ fclose($fp);
 <html lang="ja">
 
 <head>
-    <title>出版・制作関連 | Things that I (We) owned</title>
+    <title>印刷物 | Things that I (We) owned</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css" />
