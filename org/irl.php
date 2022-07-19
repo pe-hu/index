6a81b7810b3cddee898df1f7ecb89c6388f365a0
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-20 01:22:00 +0900

    org

--- org/irl.php
+++ org/irl.php
@@ -161,7 +161,7 @@ fclose($fp);
             transform: translate(-50%, -50%);
         }
 
-        #images:nth-child(n + 15) {
+        #images:nth-child(n + 25) {
             display: none;
         }
         
