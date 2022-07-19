4cf90b3d4070994cdebf5fadd1898d80330e776d
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-20 01:14:53 +0900

    Update collection.php

--- org/collection.php
+++ org/collection.php
@@ -47,8 +47,8 @@ fclose($fp);
             justify-content: space-between;
             -webkit-align-items: stretch;
             align-items: stretch;
-            -webkit-flex-wrap: wrap-reverse;
-            flex-wrap: wrap-reverse;
+            -webkit-flex-direction: row-reverse;
+            flex-direction: row-reverse;
             list-style-type: none;
         }
         
