33ce075e7a6d98a5b89699a99c8ec497dd114cfc
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-21 01:17:33 +0900

    org

--- org/niceshopsu.php
+++ org/niceshopsu.php
@@ -28,7 +28,7 @@ fclose($fp);
 <html lang="ja">
 
 <head>
-    <title>ナイスショップスー | Things that I (We) owned</title>
+    <title>gallery, community space | Things that I (We) owned</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css" />
--- org/nishitemma.php
+++ org/nishitemma.php
@@ -28,7 +28,7 @@ fclose($fp);
 <html lang="ja">
 
 <head>
-    <title>西天満のペフ | Things that I (We) owned</title>
+    <title>creative, community space | Things that I (We) owned</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css" />
