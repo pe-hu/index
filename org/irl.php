47e6581a31180e2f141af6f338bd036be071ae61
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-21 02:43:15 +0900

    Update irl.php

--- org/irl.php
+++ org/irl.php
@@ -134,7 +134,7 @@ fclose($fp);
             margin: 0.5rem 0;
         }
         
-        #about {
+        #infomation {
             width: 55rem;
             max-width: 90%;
         }
@@ -394,7 +394,7 @@ fclose($fp);
             </p>
         </div>
         <ul class="mousedragscrollable">
-            <li id="about" class="collection"></li>
+            <li id="infomation" class="collection"></li>
             <li id="otobuilding" class="collection"></li>
             <li id="nishitemma" class="collection"></li>
             <li id="niceshopsu" class="collection"></li>
@@ -437,7 +437,7 @@ fclose($fp);
     });
 
     $(function() {
-        $("#about").load("about.php");
+        $("#infomation").load("about.php");
         $("#img").load("collection.php");
         $("#otobuilding").load("otobuilding.php");
         $("#nishitemma").load("nishitemma.php");
