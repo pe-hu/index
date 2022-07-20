ae7b019ffd05bf04c870c45519a6b0d0d5c03a9f
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-21 02:30:30 +0900

    org

--- org/otobuilding.php
+++ org/otobuilding.php
@@ -28,7 +28,7 @@ fclose($fp);
 <html lang="ja">
 
 <head>
-    <title>OTO Building | Things that I (We) owned</title>
+    <title>OTO Building | The Things ∧° ┐ Own</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css" />
