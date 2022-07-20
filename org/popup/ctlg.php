242342af39291782d89a25994ef8701f88c82155
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-20 18:45:11 +0900

    Update ctlg.php

--- org/popup/ctlg.php
+++ org/popup/ctlg.php
@@ -31,11 +31,15 @@ fclose($fp);
     <title>VG CTLG | Things that I (We) owned</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css" />
-    <link rel="stylesheet" href="searchBox.css" />
+    <link rel="stylesheet" href="/org/index.css" />
+    <link rel="stylesheet" href="/org/searchBox.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+<style type="text/css">
     <style>
         #ctlg {
             position: relative;
+            font-family:'Great Vibes', "MS Mincho", serif, cursive;
         }
 
         #ctlg h2,
