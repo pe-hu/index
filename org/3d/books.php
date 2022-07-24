d9ccc6cb00b5579f3961d072cddc858aea661525
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-24 15:36:58 +0900

    Update books.php

--- org/3d/books.php
+++ org/3d/books.php
@@ -28,7 +28,7 @@ fclose($fp);
 <html lang="ja">
 
 <head>
-    <title>gallery, community space | The Things ∧° ┐ Own</title>
+    <title>Books and Zines | The Things ∧° ┐ Own</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css" />
