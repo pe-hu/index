7f0caeb8b6d93a3c7d053c5d638a9dfc89cc2366
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-27 14:31:22 +0900

    Update 3d.php

--- org/3d.php
+++ org/3d.php
@@ -93,7 +93,8 @@ fclose($fp);
         <h2><span class="pehu">∧°┐</span> が 所有するもの in 3D</h2>
         <p>このページに、∧° ┐ が 所有するもの（出版物・制作物、ウェブドメイン・デジタルツール、メディアファイルなど）を、記録します。</p>
         <p>品目に、<span class="bna"></span> の 表記があるもの を BnA Alter Meuseum で 展示・販売しています。</p>
-        <p>詳細 : <a href="https://bnaaltermuseum.com/event/the-things-i-we-own/" target="_blank">bnaaltermuseum.com/event/the-things-i-we-own/</a></p>    
+        <p>詳細 : <a href="https://bnaaltermuseum.com/event/the-things-i-we-own/" target="_blank">bnaaltermuseum.com/event/the-things-i-we-own/</a></p>
+        <br/>
         <p>左にスワイプし閲覧できるすべてのリスト内の項目は、統一のカテゴリーによって絞り込むことができます。</p>
         <br/>
         <p><i>リスト内の品目を絞り込むカテゴリーについて</i></p>
