b889338502812c35bfdaf50f090147cf0b65eef3
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-28 12:24:48 +0900

    org

--- org/collection.php
+++ org/collection.php
@@ -31,7 +31,8 @@ fclose($fp);
     <title>The Things ∧° ┐ Own</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="stylesheet.css" />
+    <link rel="stylesheet" href="/org/index.css" />
+    <link rel="stylesheet" href="/org/searchBox.css" />
     <style>
         body,
         ol,
